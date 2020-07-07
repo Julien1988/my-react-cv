d6ccb6a84c2491c50decaa45e355f8e5d4f77262
Julien <julienbroens@gmail.com>
2020-07-07 14:56:34 +0200

    bug fixed

---
+++ src/components/cv.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cv = () => {
+  return (
+    <div>
+      <p>Hello !</p>
+    </div>
+  );
+};
+
+export default Cv;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Cv from "../components/cv";
+import Cv from "./components/cv";
+
+console.log("coucou");
 
 ReactDOM.render(<Cv />, document.querySelector("#app"));
